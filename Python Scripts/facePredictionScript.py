95d9bdf8445fa4a66a2e5882e65c99747f941e4e
rahulydav <raahul.ydav@gmail.com>
2020-04-26 16:03:35 +0530

    prediction file changed

--- Python Scripts/facePredictionScript.py
+++ Python Scripts/facePredictionScript.py
@@ -1,5 +1,11 @@
+# develop a classifier for the 5 Celebrity Faces Dataset
 try:
-    from numpy import load
+    import sys
+	from numpy import load
+    from keras.models import load_model
+    from mtcnn.mtcnn import MTCNN
+    from PIL import Image
+    from numpy import asarray
     from numpy import expand_dims
     from sklearn.preprocessing import LabelEncoder
     from sklearn.preprocessing import Normalizer
@@ -7,10 +13,39 @@ try:
     from matplotlib import pyplot
 except ImportError:
     print("There was a problem in importing one or more modules.")
+        
+
+############ GLOBAL SCOPE ##############
+
+#loading face embeddings
+data = load('my-dataset-embeddings.npz')
+trainX, trainy, testX, testy = data['arr_0'], data['arr_1'], data['arr_2'], data['arr_3']
+
+# normalize input vectors
+in_encoder = Normalizer(norm='l2')
+trainX = in_encoder.transform(trainX)
+testX = in_encoder.transform(testX)
+
+#label encode targets
+out_encoder = LabelEncoder()
+out_encoder.fit(trainy)
+trainy = out_encoder.transform(trainy)
+testy = out_encoder.transform(testy)
+
+# loading SVC model
+model =  SVC(kernel='linear', probability=True)
+model.fit(trainX, trainy)
+print('svc loaded')
+
+#loading keras model
+keras_model = load_model('facenet_keras.h5')
+print('Loaded facenet Model')
+
+############ END OF GLOBAL SCOPE ############
 
 
-# function get_embedding
-def get_embedding(keras_model, face_pixels):
+# function to get_embedding
+def get_embedding(face_pixels):
     
     face_pixels = face_pixels.astype('float32')
    
@@ -25,13 +60,13 @@ def get_embedding(keras_model, face_pixels):
     yhat = keras_model.predict(samples)
     return yhat[0]
 
-# function draw_faces
-# draw each face separately
+
+# function draw_faces to draw each face separately
 def draw_faces(filename, result_list, required_size=(160, 160)):
     
     if (len(result_list)==0):
        print('no face detected')
-       return
+       return []
        
     # load the image
     data = pyplot.imread(filename)
@@ -46,6 +81,13 @@ def draw_faces(filename, result_list, required_size=(160, 160)):
         image = image.resize(required_size)
         face_array = asarray(image)
         myList.append(face_array)
+        # define subplot
+        pyplot.subplot(1, len(result_list), i+1)
+        pyplot.axis('off')
+        # plot face
+        pyplot.imshow(face_array)
+    print('detected faces: ')
+    pyplot.show()
     return myList
         
         
@@ -60,40 +102,13 @@ def detect_faces(filename):
     return draw_faces(filename, faces)
 
 
-# loading SVC classifier        
-def load_svc(trainX, trainy):
-    model = SVC(kernel='linear', probability=True)
-    print('loaded SVC')
-    model.fit(trainX, trainy)
-    return model
-        
-        
-#function load_face_embedding
-def load_face_embedding():
-    data = load('5-celebrity-faces-embeddings.npz')
-    trainX, trainy, testX, testy = data['arr_0'], data['arr_1'], data['arr_2'], data['arr_3']
-    # normalize input vectors
-    in_encoder = Normalizer(norm='l2')
-    trainX = in_encoder.transform(trainX)
-    testX = in_encoder.transform(testX)
-    # label encode targets
-    out_encoder = LabelEncoder()
-    out_encoder.fit(trainy)
-    trainy = out_encoder.transform(trainy)
-    testy = out_encoder.transform(testy)
-    return trainX, trainy, testX, testy
-
 
 #function to predict faces
 def prediction(myList):
     
-    if myList.size==0:
-        print('no faces to predict')
-        return
-    
     for test_face in myList:
         #embedding for each face to compare and predict
-        embedding = get_embedding(keras_model, test_face)
+        embedding = get_embedding(test_face)
         samples = expand_dims(embedding, axis=0)
         yhat_class = model.predict(samples)
         yhat_prob = model.predict_proba(samples)
@@ -101,7 +116,9 @@ def prediction(myList):
         # get name and probability
         class_index = yhat_class[0]
         class_probability = yhat_prob[0,class_index] * 100
+
         predict_names = out_encoder.inverse_transform(yhat_class)
+#         print('predict_name: ', predict_names)
         print('Predicted: %s (%.3f)' % (predict_names[0], class_probability))
 
         # plot for fun
@@ -113,27 +130,19 @@ def prediction(myList):
     
 
 def main():
-    #loading face embeddings
-    trainX, trainy, testX, testy = load_face_embedding()
-
-    # loading SVC model
-    model =  load_svc(trainX, trainy)
-
+    
     #list to store array of detected_faces
-    filename = 'example2.jpg'
+    filename = 'specialTest02.jpg'
     faceList = detect_faces(filename)
     if(len(faceList)==0):
        sys.exit('no face to predict...aborting')
     faceList = asarray(faceList)
-    print(faceList.shape)
-
-    #loading keras model
-    keras_model = load_model('facenet_keras.h5')
-    print('Loaded Model')
-    
+    print('shape of list containing detected faces: ', faceList.shape)
+      
     #predicting faces
     prediction(faceList)
 
     
 if __name__=="__main__":
     main()
+    print('done')
