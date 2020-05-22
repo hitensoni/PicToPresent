fc2426ea6b07eac2cbda332f0946918d9c565dd9
Hiten Soni <hitensoni14@gmail.com>
2020-05-22 21:23:15 +0530

    added UI for main activity

--- PicToPresent/app/src/main/java/com/example/pictopresent/MainActivity.java
+++ PicToPresent/app/src/main/java/com/example/pictopresent/MainActivity.java
@@ -1,15 +1,109 @@
 package com.example.pictopresent;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    final int REQUEST_CAMERA = 1;
+    final int REQUEST_GALLERY = 2;
+    final int CAMERA_RESULT = 3;
+    final int GALLERY_RESULT = 4;
+    ImageView ivCamera, ivGallery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        // trial based comment
+        initializeViews();
+    }
+
+    private void initializeViews() {
+        ivCamera = (ImageView) findViewById(R.id.ivCamera);
+        ivGallery = (ImageView) findViewById(R.id.ivGallery);
+    }
+
+    public void onClick(View view) {
+        if (view.getId() == R.id.ivCamera) {
+            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA);
+            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+                return;
+            Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+            startActivityForResult(intent, CAMERA_RESULT);
+        }
+        if (view.getId() == R.id.ivGallery) {
+            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_GALLERY);
+            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+                return;
+            Intent intent = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent, GALLERY_RESULT);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_CAMERA) {
+            if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                Toast.makeText(getApplicationContext(), "Please allow to use camera!", Toast.LENGTH_SHORT).show();
+            }
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                startActivityForResult(intent, CAMERA_RESULT);
+            }
+        }
+        if (requestCode == REQUEST_GALLERY) {
+            if (grantResults[0] == PackageManager.PERMISSION_DENIED)
+                Toast.makeText(getApplicationContext(), "Please allow to read from gallery!", Toast.LENGTH_SHORT).show();
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                Intent intent = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, GALLERY_RESULT);
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+            if(requestCode==CAMERA_RESULT){
+
+            }
+            if(requestCode==GALLERY_RESULT){
+
+            }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch(item.getItemId()){
+            case R.id.showMenu  :
+                                    break;
+            case R.id.editMenu  :
+                                    break;
+            case R.id.aboutMenu :
+                                    break;
+            case R.id.developerMenu :
+                                    break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
