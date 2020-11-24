d39e2170752f91e39aaae60848611c8abbed2761
Rahul Yadav <raahul.ydav@gmail.com>
2020-11-24 14:44:24 +0530

    Revert "file provider added (#7)" (#9)
    This reverts commit e1cae25e84e831f01323e4ceb4dba0cc46e6b534.

--- PicToPresent/app/src/main/java/com/example/pictopresent/MainActivity.java
+++ PicToPresent/app/src/main/java/com/example/pictopresent/MainActivity.java
@@ -3,10 +3,7 @@ package com.example.pictopresent;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.MediaStore;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -18,39 +15,20 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
-import androidx.core.content.FileProvider;
-
-import java.io.File;
-import java.io.IOException;
-import java.net.PasswordAuthentication;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
-    final int REQUEST_PERMISSIONS = 1;
-    final int CAM_INTENT = 3;
-    final int GALLERY_INTENT = 4;
+    final int REQUEST_CAMERA = 1;
+    final int REQUEST_GALLERY = 2;
+    final int CAMERA_RESULT = 3;
+    final int GALLERY_RESULT = 4;
     ImageView ivCamera, ivGallery;
-    private Uri imageUri = null;
-    String imageFilePath = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initializeViews();
-        permissions();
-    }
-
-    private void permissions() {
-        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED ||
-                ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ||
-                ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
-            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA,
-                    Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_PERMISSIONS);
     }
 
     private void initializeViews() {
@@ -58,51 +36,52 @@ public class MainActivity extends AppCompatActivity {
         ivGallery = (ImageView) findViewById(R.id.ivGallery);
     }
 
-    public void clickImage(View view){
-        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        File imageFile = null;
-        try{
-            imageFile = getImageFile();
-        }catch (IOException e){
-            Toast.makeText(getApplicationContext(), "Photo file can't be created!", Toast.LENGTH_SHORT).show();
-            return;
+    public void onClick(View view) {
+        if (view.getId() == R.id.ivCamera) {
+            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.CAMERA}, REQUEST_CAMERA);
+            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+                return;
+            Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+            startActivityForResult(intent, CAMERA_RESULT);
         }
-        if(imageFile!=null){
-            imageUri = FileProvider.getUriForFile(this,  "com.example.pictopresent.provider", imageFile);
-            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-            startActivityForResult(cameraIntent, CAM_INTENT);
+        if (view.getId() == R.id.ivGallery) {
+            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, REQUEST_GALLERY);
+            if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)
+                return;
+            Intent intent = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+            startActivityForResult(intent, GALLERY_RESULT);
         }
-    }
-
-    private File getImageFile() throws IOException{
-        String timestamp = new SimpleDateFormat("yyyyMMddHHmmss").format(new Date());
-        String filename = "IMG_" + timestamp;
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
-        File image = File.createTempFile(filename, ".jpg", storageDir);
-        imageFilePath = image.getAbsolutePath();
-        return image;
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
-        if(requestCode==REQUEST_PERMISSIONS) {
-            if (grantResults[0] == PackageManager.PERMISSION_DENIED || grantResults[1] == PackageManager.PERMISSION_DENIED
-                    || grantResults[2] == PackageManager.PERMISSION_DENIED) {
-                Toast.makeText(getApplicationContext(), "Please allow necessary permissions to continue!", Toast.LENGTH_SHORT).show();
-                finish();
+        if (requestCode == REQUEST_CAMERA) {
+            if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                Toast.makeText(getApplicationContext(), "Please allow to use camera!", Toast.LENGTH_SHORT).show();
+            }
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+                startActivityForResult(intent, CAMERA_RESULT);
+            }
+        }
+        if (requestCode == REQUEST_GALLERY) {
+            if (grantResults[0] == PackageManager.PERMISSION_DENIED)
+                Toast.makeText(getApplicationContext(), "Please allow to read from gallery!", Toast.LENGTH_SHORT).show();
+            if(grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                Intent intent = new Intent(Intent.ACTION_PICK,android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                startActivityForResult(intent, GALLERY_RESULT);
             }
         }
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-            if(requestCode==CAM_INTENT){
-                Intent intent = new Intent(this, com.example.pictopresent.PredictionActivity.class);
-                intent.putExtra("image_uri", imageUri);
-                startActivity(intent);
-                finish();
+            if(requestCode==CAMERA_RESULT){
+
             }
-            if(requestCode==GALLERY_INTENT){
+            if(requestCode==GALLERY_RESULT){
 
             }
     }
